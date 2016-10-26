922a908b56feac74be588da64c8557a379e10f60
Elisabeth Henry <liz.henry@ouvaton.org>
2016-10-26 12:39:43 +0200

    Fix comments so localize_macros.rs can be incuded from OUT_DIR

--- data/localize_macros.rs
+++ data/localize_macros.rs
@@ -1,16 +1,16 @@
-//! This file was generated automatically by crowbook-localize.
-//! It is probably not a good idea to edit it manually.
-//!
-//! # Usage:
-//!
-//! ```rust, no_run
-//! #[macro_use] mod localize_macros;
-//! use localize_macros::set_lang;
-//! set_lang("en");
-//! lformat!("Hello, {}", name);
-//! set_lang("fr");
-//! lformat!("Hello, {}", name);
-//! ```
+// This file was generated automatically by crowbook-localize.
+// It is probably not a good idea to edit it manually.
+//
+// # Usage:
+//
+// ```rust, no_run
+// #[macro_use] mod localize_macros;
+// use localize_macros::set_lang;
+// set_lang("en");
+// lformat!("Hello, {}", name);
+// set_lang("fr");
+// lformat!("Hello, {}", name);
+// ```
 
 use std::sync::RwLock;
 use std::sync::RwLockReadGuard;
